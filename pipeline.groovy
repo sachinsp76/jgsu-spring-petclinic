9a568f45330b9fa415b77b460e121b932229dcb4
Sachin Pednekar <sachinsp76@gmail.com>
2021-07-28 20:47:34 +0530

    Pushing pipeline.groovy onto Git repo

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                bat './mvnw.cmd clean package'
+                bat './mvnw.cmd clean'
             }
 
             post {
